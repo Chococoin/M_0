1b0a0b9757b9a83f406c9e07166e4d14df607e9b
German G. Lugo Munoz <glugo-mu@42firenze.it>
2024-11-29 19:14:01 +0100

    Add ft.

---
+++ ft_memcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: glugo-mu <glugo-mu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/27 19:51:12 by glugo-mu          #+#    #+#             */
+/*   Updated: 2024/11/29 19:13:34 by glugo-mu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	char		*d;
+	const char	*s;
+	size_t		i;
+
+	if (!dest || !src)
+		return (NULL);
+	s = (const char *)src;
+	d = (char *)dest;
+	i = 0;
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (dest);
+}
