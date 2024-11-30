bc3dfe61b9cf00f36e1b0ff221ee6896fc6866f8
German G. Lugo Munoz <glugo-mu@42firenze.it>
2024-11-30 09:16:45 +0100

    Add ft.

---
+++ ft_memmove.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: glugo-mu <glugo-mu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/29 22:26:51 by glugo-mu          #+#    #+#             */
+/*   Updated: 2024/11/30 09:15:48 by glugo-mu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char		*d;
+	const unsigned char	*s;
+	size_t				i;
+
+	if (!dest || !src)
+		return (NULL);
+	d = (unsigned char *)dest;
+	s = (const unsigned char *)src;
+	if (d < s)
+	{
+		i = 0;
+		while (++i < n)
+		{
+			d[i] = s[i];
+		}
+	}
+	else
+	{
+		i = n;
+		while (i-- > 0)
+		{
+			d[i] = s[i];
+		}
+	}
+	return (dest);
+}
