0bb4414a662493254aa08e9d6e139e27587ced76
German G. Lugo Munoz <glugo-mu@42firenze.it>
2024-11-30 17:28:38 +0100

    Add ft.

---
+++ ft_memchr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: glugo-mu <glugo-mu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/30 14:52:30 by glugo-mu          #+#    #+#             */
+/*   Updated: 2024/11/30 17:25:51 by glugo-mu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char	*s1;
+	unsigned char	c1;
+	size_t			i;
+
+	s1 = (unsigned char *)s;
+	c1 = (unsigned char)c;
+	i = 0;
+	while (i < n)
+	{
+		if (s1[i] == c1)
+		{
+			return (&s1[i]);
+		}
+		i++;
+	}
+	return (NULL);
+}
