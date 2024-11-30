60fd1da6f187312498b09a0b2acbde463ee8d47a
German G. Lugo Munoz <glugo-mu@42firenze.it>
2024-11-30 10:25:07 +0100

    Add ft.

---
+++ ft_strncmp.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: glugo-mu <glugo-mu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/30 09:24:10 by glugo-mu          #+#    #+#             */
+/*   Updated: 2024/11/30 10:24:11 by glugo-mu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+int	strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	if (n == 0)
+		return (0);
+	i = 0;
+	while ((s1[i] || s2[i]) && n > i)
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	return (0);
+}
