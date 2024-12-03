92537b7120f4ffc747e511683dfe2c2869cee3ac
German G. Lugo Munoz <glugo-mu@42firenze.it>
2024-12-03 11:07:50 +0100

    Add ft.

---
+++ ft_substr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: glugo-mu <glugo-mu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/01 17:40:25 by glugo-mu          #+#    #+#             */
+/*   Updated: 2024/12/03 11:07:02 by glugo-mu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+#include <stdlib.h>
+#include "libft.h"
+#include <stdio.h>
+
+char	*ft_subst(char const *s, unsigned int start, size_t len)
+{
+	char	*buffer;
+	size_t	s_len;
+	size_t	i;
+
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+		return (ft_strdup(""));
+	if (len > s_len - start)
+		len = s_len - start;
+	buffer = (char *)malloc(sizeof(char) * (len + 1));
+	if (!buffer)
+		return (0);
+	i = 0;
+	while (i < len)
+	{
+		buffer[i] = s[start + i];
+		i++;
+	}
+	buffer[i] = '\0';
+	return (buffer);
+}
