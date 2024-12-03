ac87371a736800fa6e579a008aa9584f5ff0f96f
German G. Lugo Munoz <glugo-mu@42firenze.it>
2024-12-03 23:24:17 +0100

    Add ft.

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,91 +6,94 @@
 /*   By: glugo-mu <glugo-mu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 20:11:53 by glugo-mu          #+#    #+#             */
-/*   Updated: 2024/12/03 21:21:25 by glugo-mu         ###   ########.fr       */
+/*   Updated: 2024/12/03 22:59:30 by glugo-mu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "libft.h"
 
-static int ft_count_words(char const *s, char c)
+static int	ft_count_words(char const *s, char c)
 {
-	int nwords = 0;
-	int isword = 0;
-	int i;
-	int len;
+	int	nwords;
+	int	isword;
+	int	i;
+	int	len;
 
 	len = strlen(s);
 	nwords = 0;
 	isword = 0;
 	i = 0;
-	while(i < len)
-	{	
+	while (i < len)
+	{
 		if (s[i] != c && !isword)
 		{
 			isword = 1;
 			nwords++;
 		}
 		if (s[i] == c && isword)
-		{
 			isword = 0;
-		}
 		i++;
 	}
 	return (nwords);
 }
-static char **ft_copy_and_paste_words(char const *s, char c, const char **bffr)
+
+static char	**ft_copy_and_paste_words(char const *s, char c, char **bffr)
 {
-	int i;
-	char **buffer = bffr;
+	int		i;
+	int		j;
+	int		start;
+
+	i = 0;
+	j = 0;
 	while (s[i])
 	{
-		if (s[i] != c)
-			printf("%c", s[i]);
-		i++;
+		while (s[i] == c)
+			i++;
+		start = i;
+		while (s[i] && s[i] != c)
+			i++;
+		if (i > start)
+		{
+			bffr[j] = ft_strdup(&s[start]);
+			if (!bffr[j])
+				return (NULL);
+			bffr[j][i - start] = '\0';
+			j++;
+		}
 	}
-	
-	return (buffer);
+	bffr[j] = NULL;
+	return (bffr);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	int	nwords;
-	char **buff;
+	int		nwords;
+	char	**buff;
 
 	nwords = ft_count_words(s, c);
-	printf("Num of word: %d\n", nwords);
-	buff = (char **)malloc(sizeof(char) * (nwords + 1));
-	if (!buff || nwords == 0)
+	buff = (char **)malloc(sizeof(char *) * (nwords + 1));
+	if (!buff)
 		return (NULL);
+	if (nwords == 0)
+	{
+		buff[0] = ft_strdup("");
+		buff[1] = NULL;
+		return (buff);
+	}
 	buff = ft_copy_and_paste_words(s, c, buff);
 	return (buff);
 }
-int main()
-{
-	char *s1 = "---Hola--mundo---bello--hermoso.--";
-	char c = '-';
-	char **nwords = ft_split(s1, c);
-	for (int i = 0; nwords[i] != NULL; i++) {
-        printf("Word %d: %s\n", i, nwords[i]);
-    }
-	// char **result;
-	// result = (char **)malloc((nwords + 1) * sizeof(char *));
-	// int i = 0;
-	// int start;
-	// int j = 0;
-	// while (s1[i])
-	// {
-	// 	while (s1[i] == c)
-	// 		i++;
-	// 	start = i;
-	// 	while (s1[i] != c)
-	// 		i++;
-	// 	if (i > start)
-	// 	{
-	// 		result[j++] = 'a';
-	// 	}
-	// }
-	return (0);
-}
+
+// int	main()
+// {
+// 	char *s1 = "               ";
+// 	char c = ' ';
+// 	char **nwords = ft_split(s1, c);
+// 	for (int i = 0; nwords[i] != NULL; i++) {
+// 		printf("Word %d: %s\n", i, nwords[i]);
+// 	}
+// 	return (0);
+// }
