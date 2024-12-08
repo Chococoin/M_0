a9e9228345944425739d4eef65c0271adaeaea73
German G. Lugo Munoz <glugo-mu@42firenze.it>
2024-12-08 17:53:17 +0100

    Bugged ft.

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,11 +6,10 @@
 /*   By: glugo-mu <glugo-mu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 13:50:07 by glugo-mu          #+#    #+#             */
-/*   Updated: 2024/12/03 20:32:39 by glugo-mu         ###   ########.fr       */
+/*   Updated: 2024/12/08 17:50:41 by glugo-mu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include "libft.h"
 
 static int	ft_is_in_set(char c, char const *set)
@@ -69,4 +68,45 @@ char	*ft_strtrim(char const *s1, char const *set)
 	}
 	result[end] = '\0';
 	return (result);
+}
+int	main(void)
+{
+	char	*test_cases[6] = {
+		"  Hello World  ",
+		"xxHello Worldxx",
+		"!!Hello World!!",
+		"  !!Hello World!!  ",
+		"NoTrimNeeded",
+		"   "
+	};
+	char	*sets[6] = {
+		" ",
+		"x",
+		"!",
+		" !",
+		"",
+		NULL
+	};
+	char	*expected_results[6] = {
+		"Hello World",
+		"Hello World",
+		"Hello World",
+		"Hello World",
+		"NoTrimNeeded",
+		""
+	};
+	int		i;
+	char	*result;
+
+	for (i = 0; i < 6; i++)
+	{
+		result = ft_strtrim(test_cases[i], sets[i]);
+		printf("Test %d:\n", i + 1);
+		printf("Input: '%s', Set: '%s'\n", test_cases[i], sets[i]);
+		printf("Expected: '%s'\n", expected_results[i]);
+		printf("Result: '%s'\n", result);
+		printf("Match: %s\n\n", (strcmp(result, expected_results[i]) == 0) ? "Yes" : "No");
+		free(result);
+	}
+	return (0);
 }
