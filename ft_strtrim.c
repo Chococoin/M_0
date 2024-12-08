0deae1c15529b150872299030c738e309d6eb1b7
German G. Lugo Munoz <glugo-mu@42firenze.it>
2024-12-08 19:32:06 +0100

    Fix ft.

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,107 +6,90 @@
 /*   By: glugo-mu <glugo-mu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 13:50:07 by glugo-mu          #+#    #+#             */
-/*   Updated: 2024/12/08 18:36:15 by glugo-mu         ###   ########.fr       */
+/*   Updated: 2024/12/08 19:24:51 by glugo-mu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_is_in_set(char c, char const *set)
+static int	ft_trim_start(char const *s1, char const *set)
 {
-	while (*set != '\0')
-	{
-		if (c == *set)
-			return (1);
-		set++;
-	}
-	return (0);
+	int	start;
+
+	start = 0;
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	return (start);
 }
 
-static int	ft_get_start(char const *s1, char const *set)
+static int	ft_trim_end(char const *s1, char const *set)
 {
-	int	i;
+	int	end;
 
-	i = 0;
-	while (s1[i] != '\0' && ft_is_in_set(s1[i], set))
-		i++;
-	return (i);
-}
-
-static int	ft_get_end(char const *s1, char const *set, int const start)
-{
-	int	len;
-
-	len = ft_strlen(s1);
-	while (len > start && ft_is_in_set(s1[len -1], set))
-	{
-		len--;
-	}
-	return (len);
+	end = ft_strlen(s1) - 1;
+	while (end >= 0 && ft_strchr(set, s1[end]))
+		end--;
+	return (end);
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	int		start;
 	int		end;
-	char	*result;
-	int		i;
+	char	*trimmed;
 
-	if (s1 == NULL || set == NULL)
+	if (!s1 || !set)
 		return (NULL);
-	start = ft_get_start(s1, set);
-	end = ft_get_end(s1, set, start);
-	result = (char *)malloc(sizeof(char) * (end - start + 1));
-	if (!result)
+	start = ft_trim_start(s1, set);
+	end = ft_trim_end(s1, set);
+	if (start > end)
+		return (ft_strdup(""));
+	trimmed = malloc((end - start + 2) * sizeof(char));
+	if (!trimmed)
 		return (NULL);
-	i = 0;
-	while (start <= (end -1))
-	{
-		result[i] = s1[start];
-		start++;
-		i++;
-	}
-	result[end] = '\0';
-	return (result);
+	ft_strlcpy(trimmed, s1 + start, end - start + 2);
+	return (trimmed);
 }
-int main(void)
-{
-	const char *test_cases[] = {
-		"  Hello World  ",
-		"  Hello World",
-		"Hello World  ",
-		"Hello World",
-		"NoTrimNeeded",
-		""
-	};
-	const char *sets[] = {
-		" ",
-		" ",
-		" ",
-		" ",
-		" ",
-		" "
-	};
-	const char *expected_results[] = {
-		"Hello World",
-		"Hello World",
-		"Hello World",
-		"Hello World",
-		"NoTrimNeeded",
-		""
-	};
-	int i;
-	char *result;
 
-	for (i = 0; i < 5; i++)
-	{
-		result = ft_strtrim(test_cases[i], sets[i]);
-		printf("Test %d:\n", i + 1);
-		printf("Input: '%s', Set: '%s'\n", test_cases[i], sets[i]);
-		printf("Expected: '%s'\n", expected_results[i]);
-		printf("Result: '%s'\n", result);
-		printf("Match: %s\n\n", (strcmp(result, expected_results[i]) == 0) ? "Yes" : "No");
-		free(result);
-	}
-	return 0;
-}
+// int main(void)
+// {
+// 	const char *test_cases[] = {
+// 		"  Hello World  ",
+// 		"  Hello World",
+// 		"Hello World  ",
+// 		"Hello World",
+// 		"NoTrimNeeded",
+// 		""
+// 	};
+// 	const char *sets[] = {
+// 		" ",
+// 		" ",
+// 		" ",
+// 		" ",
+// 		" ",
+// 		" "
+// 	};
+// 	const char *expected_results[] = {
+// 		"Hello World",
+// 		"Hello World",
+// 		"Hello World",
+// 		"Hello World",
+// 		"NoTrimNeeded",
+// 		""
+// 	};
+// 	int i;
+// 	char *result;
+
+// 	for (i = 0; i < 5; i++)
+// 	{
+// 		result = ft_strtrim(test_cases[i], sets[i]);
+// 		printf("Test %d:\n", i + 1);
+// 		printf("Input: '%s', Set: '%s'\n", test_cases[i], sets[i]);
+// 		printf("Expected: '%s'\n", expected_results[i]);
+// 		printf("Result: '%s'\n", result);
+// 		printf("Match: %s\n\n", (strcmp(result, expected_results[i]) == 0) ? 
+// "Yes" : "No");
+// 		free(result);
+// 	}
+// 	return 0;
+// }
