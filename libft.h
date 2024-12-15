23cc2fb5cb821a060fcf4f73a1b3b09ee68c1c06
German G. Lugo Munoz <glugo-mu@42firenze.it>
2024-12-15 16:48:29 +0100

    Fix header.

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: glugo-mu <glugo-mu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 15:41:15 by glugo-mu          #+#    #+#             */
-/*   Updated: 2024/12/14 20:51:29 by glugo-mu         ###   ########.fr       */
+/*   Updated: 2024/12/15 16:41:08 by glugo-mu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,6 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-char	*ft_strrchr(const char *s, int c);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
